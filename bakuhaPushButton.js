fe105c49c3475532d1529eb7343366699f0e8817
Ha10gen <70700073+Ha10gen@users.noreply.github.com>
2025-09-29 22:40:19 +0900

    Update bakuhaPushButton.js

--- bakuhaPushButton.js
+++ bakuhaPushButton.js
@@ -1,34 +1,26 @@
 const btn = document.getElementById('soundBtn');
 const img = document.getElementById('pushButton');
 const player = document.getElementById('boomSound');
+const volumeSlider = document.getElementById('volumeSlider');
 const normalSrc = 'war_bakuha_switch_off.png';
 const activeSrc = 'war_bakuha_switch_on.png';
 const soundSrc = 'boom.wav';
 
-// 初回準備フラグ
-let audioReady = false;
-
 // Audioオブジェクトを1回だけ作成
-const sound = new Audio(soundSrc);
+var audio_key = new Audio(soundSrc);
+var audio_volume = volumeSlider.value / 100;
 
-// 再生関数
-function playSound() {
-    if (!audioReady) {
-        // 初回クリックでユーザー操作として準備
-        sound.play().then(() => {
-            sound.pause();
-            sound.currentTime = 0;
-            audioReady = true;
-            // 本来の再生
-            sound.play();
-        }).catch(err => console.warn('再生失敗:', err));
-    } else {
-        sound.currentTime = 0;
-        sound.play().catch(err => console.warn('再生失敗:', err));
-    }
+function playSound(){
+    audio_key = new Audio(soundSrc);
+    audio_key.volume = volumeSlider.value / 100;
+    audio_key.play().catch(err=>console.warn('再生失敗:', err));
 }
 
-
+volumeSlider.addEventListener('input', () => {
+  const val = volumeSlider.value;
+  player.volume = val / 100;
+  volumeValue.textContent = val; // 数値表示
+});
 
 btn.addEventListener('mousedown', ()=>{
     img.src = activeSrc;
